6e84dc90038472512473a9543c4711db77151bc5
Darius Mejeras <darius.mejeras@gmail.com>
2021-10-29 17:51:24 +0300

    Remove copyrighted code

--- parse.go
+++ parse.go
@@ -1,9 +1,9 @@
 package untold
 
 import (
+	"fmt"
 	"github.com/pkg/errors"
 	"reflect"
-	"strconv"
 	"strings"
 )
 
@@ -74,67 +74,21 @@ func doParse(reflection reflect.Value, resolve resolveFn) error {
 	return nil
 }
 
-// https://github.com/fatih/structtag/blob/master/tags.go
 func tagName(tag reflect.StructTag, target string) string {
-	for tag != "" {
-		// Skip leading space.
-		i := 0
-		for i < len(tag) && tag[i] == ' ' {
-			i++
-		}
-		tag = tag[i:]
-		if tag == "" {
-			break
-		}
+	fmt.Println(string(tag))
+	tags := strings.Split(string(tag), " ")
+	if len(tags) == 0 {
+		return ""
+	}
 
-		// Scan to colon. A space, a quote or a control character is a syntax
-		// error. Strictly speaking, control chars include the range [0x7f,
-		// 0x9f], not just [0x00, 0x1f], but in practice, we ignore the
-		// multi-byte control characters as it is simpler to inspect the tag's
-		// bytes than the tag's runes.
-		i = 0
-		for i < len(tag) && tag[i] > ' ' && tag[i] != ':' && tag[i] != '"' && tag[i] != 0x7f {
-			i++
-		}
-
-		if i == 0 {
-			return ""
-		}
-		if i+1 >= len(tag) || tag[i] != ':' {
-			return ""
-		}
-		if tag[i+1] != '"' {
+	for i := range tags {
+		tagParts := strings.Split(tags[i], ":")
+		if len(tagParts) != 2 {
 			return ""
 		}
 
-		key := string(tag[:i])
-		tag = tag[i+1:]
-
-		// Scan quoted string to find value.
-		i = 1
-		for i < len(tag) && tag[i] != '"' {
-			if tag[i] == '\\' {
-				i++
-			}
-			i++
-		}
-		if i >= len(tag) {
-			return ""
-		}
-
-		qvalue := string(tag[:i+1])
-		tag = tag[i+1:]
-
-		value, err := strconv.Unquote(qvalue)
-		if err != nil {
-			return ""
-		}
-
-		res := strings.Split(value, ",")
-		name := res[0]
-
-		if key == target {
-			return name
+		if tagParts[0] == target {
+			return strings.Trim(tagParts[1], "\"")
 		}
 	}
 
